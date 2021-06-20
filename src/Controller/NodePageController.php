9a449c6c596775cd9fdfb6aa9d863844022ec5f5
ramesh <rameshpongalur@gmail.com>
2021-06-20 21:16:30 +0530

    code changes

--- src/Controller/NodePageController.php
+++ src/Controller/NodePageController.php
@@ -38,12 +38,12 @@ class NodePageController extends ControllerBase {
     );
   }
   /**
-  * @param $apikey - string
-  * api key value from url
-  * @param NodeInterface $node -entity
-  * node loaded from node id parameter
-  * @return JsonResponse
-  */
+   * @param $apikey - string
+   * api key value from url
+   * @param NodeInterface $node -entity
+   * node loaded from node id parameter
+   * @return JsonResponse
+   */
   public function content($apikey, NodeInterface $node)  {
     // Site API Key configuration value
     $user_apikey_entered = $this->config->getEditable('node_page_json.settings')->get('siteapikey');
